5758f91e1d83d7d6472561a01574e953fd12bca3
Priya Dharshini  S <s_priyadharshini@hcl.com>
2021-12-12 11:50:45 +0530

    first commit

---
+++ src/main/java/com/boot/DevopsGradedApplication.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGradedApplication {
+
+	public static void main(String[] args) {
+		System.out.println( "Hello DevOps" );
+	}
+
+}
